d7df3ff5dd3b6a07577ae83befb7a6a4b2328a0d
Alex-de-Lecea <100385375+Alex-de-Lecea@users.noreply.github.com>
2022-11-01 16:58:42 -0700

    Updated OLM_regression with plotting functionality

--- napeca_post/OLM_regression.py
+++ napeca_post/OLM_regression.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import predictor_matrix_gen
 
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
 #user variables
 practice_inputs = os.path.abspath('./napeca_post/sample_data/VJ_OFCVTA_7_260_D6/event_times_VJ_OFCVTA_7_260_D6_trained.csv')
 practice_outputs = os.path.abspath('./napeca_post/sample_data/VJ_OFCVTA_7_260_D6/VJ_OFCVTA_7_260_D6_neuropil_corrected_signals_15_50_beta_0.8.csv')
@@ -32,6 +35,7 @@ regress_mat = predictor_matrix_gen.predic_mat_gen(read_data, size, predic_mat, c
 
 predic_mat_size = np.shape(predic_mat)
 
+#moving average calculations
 moving_average_output = predictor_matrix_gen.moving_average(output_data[cell_number], 20)
 moving_average_pos = predictor_matrix_gen.moving_average(regress_mat[0], 20)
 moving_average_pos = predictor_matrix_gen.moving_average(regress_mat[1], 20)
@@ -44,4 +48,25 @@ olsmod = sm.OLS((moving_average_output - min), regress_mat)
 olsres = olsmod.fit()
 ypred = olsres.predict(regress_mat)
 
+#plot creation with plotly
+x = np.array(range(0, predic_mat_size[0]))
+pos_stim_arr = np.transpose(regress_mat)[0]
+neg_stim_arr = np.transpose(regress_mat)[1]
+
+fig = make_subplots(rows=1, cols=2,
+                    subplot_titles = ("Moving Average of Actual Fluoresence", "Predicted Fluoresence"),
+                    shared_yaxes=True,
+                    horizontal_spacing=0.02)
+
+fig['layout']['xaxis']['title'] = "Sample number"
+fig['layout']['xaxis2']['title'] = "Sample number"
+fig['layout']['yaxis']['title'] = "Fluoresence"
+fig.add_trace(go.Scattergl(x=x, y=(moving_average_output - min), mode='lines'), row=1, col=1)
+fig.add_trace(go.Scattergl(x=x, y=ypred, mode='lines'), row=1, col=2)
+
+fig.update_layout(height=600, width=1200)
+fig.update_xaxes(matches='x')
+
+#showing the output
+fig.show()
 print(olsres.summary())
